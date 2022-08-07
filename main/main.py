c5169e3541c453430cb2e80c03ea2c1bd5c2229e
Raj Verma <rajverma@Rupals-MacBook-Air.local>
2022-08-07 20:22:03 +0530

    snake movement added

--- main/main.py
+++ main/main.py
@@ -17,6 +17,14 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
+
+
 game_on = True
 
 while game_on:
--- snake/body.py
+++ snake/body.py
@@ -3,10 +3,12 @@ from turtle import Turtle
 START_POS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE = 20
 
+
 class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for pos in START_POS:
@@ -21,4 +23,20 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].fd(MOVE)
+        self.head.fd(MOVE)
+
+    def up(self):
+        self.head.setheading(90)
+
+    def down(self):
+        self.head.setheading(270)
+
+    def left(self):
+        self.head.setheading(180)
+
+    def right(self):
+        self.segments[0].setheading(0)
+
+
+
+
